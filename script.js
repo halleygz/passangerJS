02bb79af5b6a12caacfce908f03fa30ccd6da302
halleygz <halleluya2013idone@gmail.com>
2024-05-21 15:18:35 +0300

    made some changes in script.js

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
+//getting DOM elements
 let countEL = document.getElementById('count-el')
 let saveEl = document.getElementById('save-el')
+//initialize counter saver string
 let count = 0
 let saver = ''
+//increment function
 function increment() {
-    count+=1;
+    count++;
     countEL.textContent = count
 }
+// save function
 function save(){
     saveEl.innerText += ` ${count} - `
     count = 0
